97265c5dfcce4213fd4c5ed432b8c4b67dfec593
Govindan Vedanarayanan <govind@Govind-Air.home>
2017-06-30 13:50:24 -0400

    try.js

---
+++ public/js/try.js
@@ -0,0 +1,20 @@
+/* global TrelloPowerUp */
+
+var t = TrelloPowerUp.iframe();
+
+// Elements with IDs are available as properties of `window`.
+window.estimate.addEventListener('submit', function(event){
+  // Stop the browser trying to submit the form itself.
+  event.preventDefault();
+  return window.open('https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=8c2a4afcffb74f4faee17b68d0b3fc18');
+});
+
+t.render(function(){
+  return t.get('card', 'private', 'estimate')
+  .then(function(estimate){
+    window.newsSource.value = estimate;
+  })
+  .then(function(){
+    t.sizeTo('#estimate').done();
+  });
+});
