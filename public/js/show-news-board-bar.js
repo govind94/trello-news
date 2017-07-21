358be1a54dc52f80147db3e14211dc35345c3562
System <none>
2017-07-21 23:13:44 +0000

    🍇🌽 Updated with Glitch

--- public/js/show-news-board-bar.js
+++ public/js/show-news-board-bar.js
@@ -7,42 +7,34 @@ var category = t.arg('category');
 
 document.getElementById("title").innerHTML = "Top " + category + " stories:";
 
-document.getElementsByTagName("body")[0].setAttribute("bgcolor", "#E6E6FA");
-
 for(var key in jsonResponse.articles){
-  var li = document.createElement("li");
-  var a = document.createElement('a');
-  var child = document.createElement('div');
-  if (key !==0 ) child.style.clear = "left";
-  var p1 = document.createElement('p');
-  p1.style.float = "left";
-  var p2 = document.createElement('p');
-  var img = document.createElement('img');
   var div = document.createElement('div');
-  div.style.float = "left";
-  if (key !== 0) div.style.clear = "left";
-  var br = document.createElement('br');
+  div.className = "col-md-6 card";
+  var childDiv = document.createElement('div');
+  childDiv.className = "container";
+  var img = document.createElement('img');
   img.src = (jsonResponse.articles[key].urlToImage ? jsonResponse.articles[key].urlToImage : 'https://cdn.glitch.com/953b60d4-d622-4431-8327-7bde55bd3e5f%2Fnews_icon.png?1499092863229');
-  var linkText = document.createTextNode(jsonResponse.articles[key].title);
-  var descText = jsonResponse.articles[key].description ? jsonResponse.articles[key].description : "Description is currently unavailable. Click link for more details."
-  var description = document.createTextNode(descText);
   img.height = 100;
   img.width = 110;
-  p1.appendChild(img);
-  child.appendChild(p1);
-  p2.appendChild(description);
-  child.appendChild(p2);
-  //p.appendChild(img);
-  //p.appendChild(description);
+  var p = document.createElement('p');
+  p.className = "text-success";
+  p.style.display = "inline";
+  var descText = jsonResponse.articles[key].description ? jsonResponse.articles[key].description : "Description is currently unavailable. Click link for more details."
+  var description = document.createTextNode(descText);
+  p.appendChild(description);
+  var h4 = document.createElement('h4');
+  var a = document.createElement('a');
+  var linkText = document.createTextNode(jsonResponse.articles[key].title);
   a.appendChild(linkText);
   a.href = jsonResponse.articles[key].url;
   a.target = "_blank";
-  div.appendChild(a);
-  //div.appendChild(br);
-  div.appendChild(child);
-  li.appendChild(div);
-  document.getElementById("myList").appendChild(br);
-  document.getElementById("myList").appendChild(li);
+  a.title = jsonResponse.articles[key].title;
+  h4.appendChild(a);
+  childDiv.appendChild(h4);
+  div.appendChild(img);
+  div.appendChild(p);
+  div.appendChild(childDiv);
+  document.getElementById("test").appendChild(div);
 }
 
 document.addEventListener('click', function(e){
--- public/js/show-news-popup.js
+++ public/js/show-news-popup.js
@@ -8,28 +8,33 @@ var category = t.arg('category');
 document.getElementById("title").innerHTML = "Top " + category + " stories:";
 
 for(var key in jsonResponse.articles){
-  var li = document.createElement("li");
-  var a = document.createElement('a');
-  var p = document.createElement('p');
-  var img = document.createElement('img');
   var div = document.createElement('div');
-  var br = document.createElement('br');
+  div.className = "col-md-6 card";
+  var childDiv = document.createElement('div');
+  childDiv.className = "container";
+  var img = document.createElement('img');
   img.src = (jsonResponse.articles[key].urlToImage ? jsonResponse.articles[key].urlToImage : 'https://cdn.glitch.com/953b60d4-d622-4431-8327-7bde55bd3e5f%2Fnews_icon.png?1499092863229');
-  var linkText = document.createTextNode(jsonResponse.articles[key].title);
+  img.height = 100;
+  img.width = 110;
+  var p = document.createElement('p');
+  p.className = "text-success";
+  p.style.display = "inline";
   var descText = jsonResponse.articles[key].description ? jsonResponse.articles[key].description : "Description is currently unavailable. Click link for more details."
   var description = document.createTextNode(descText);
   p.appendChild(description);
-  img.height = 80;
-  img.width = 100;
+  var center = document.createElement('center');
+  var a = document.createElement('a');
+  var linkText = document.createTextNode(jsonResponse.articles[key].title);
   a.appendChild(linkText);
   a.href = jsonResponse.articles[key].url;
   a.target = "_blank";
-  div.appendChild(a);
+  a.title = jsonResponse.articles[key].title;
+  center.appendChild(a);
+  childDiv.appendChild(center);
+  div.appendChild(childDiv);
   div.appendChild(img);
   div.appendChild(p);
-  li.appendChild(div);
-  document.getElementById("myList").appendChild(br);
-  document.getElementById("myList").appendChild(li);
+  document.getElementById("test").appendChild(div);
 }
 
 document.addEventListener('click', function(e){
